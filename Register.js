f503b26a37a94644dad788bf355584fdf17f5086
xuanchi2004 <133880129+xuanchi2004@users.noreply.github.com>
2023-10-12 07:24:14 +0700

    Add files via upload

---
+++ Register.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('registerButton').addEventListener('click', function (event) {
+      event.preventDefault(); 
+      
+      window.location.href = "index.html";
+  });
+});
